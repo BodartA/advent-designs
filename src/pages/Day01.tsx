36990eadb5aeefa5cd7707e0b6cbae3117b44de6
BodartA <a.bodart23@gmail.com>
2024-12-05 18:22:57 +0100

    Commit before trying using grid on middle card section

--- src/pages/Day01.tsx
+++ src/pages/Day01.tsx
@@ -1,5 +1,7 @@
 import React from 'react';
 import Navbar from '../components/Navbar'
+import SantaClaus from '../assets/SantaClaus.jpg'
+import { Facebook, Linkedin, Twitter, Instagram, Dribbble } from 'lucide-react';
 
 const Day01 = () => {
   return (
@@ -13,7 +15,32 @@ const Day01 = () => {
 
         <div className='justify-center p-4 flex'>
 
-          <div className=' border-2 border-black rounded-xl flex w-[300px] h-[450px]'></div>
+          <div className='rounded-3xl flex-col w-[300px] h-[450px] bg-green-700'>
+
+            <div className='pattern-vertical-lines pattern-red-500 pattern-bg-white pattern-size-20 pattern-opacity-100 w-full h-2/6 rounded-t-3xl drop-shadow-2xl flex justify-center items-center'>
+              <img src={SantaClaus} className='h-[96px] w-[96px] rounded-full object-cover object-right drop-shadow-2xl'></img>
+            </div>
+
+            <div className='h-3/6 text-pretty text-center bg-blue-500 px-4'>
+              <div>
+                <h2 className=''>Santa Claus</h2>
+                <h3>@santaclaus</h3>
+              </div>
+
+              <div className=' flex justify-center gap-6'>
+                <Facebook size={18} />
+                <Linkedin size={18} />
+                <Twitter size={18} />
+                <Instagram size={18} />
+                <Dribbble size={18} />
+              </div>
+              <div>
+                <p>Santa Claus is the best of the best. He will give you a lot of presents of you were a very good boy during the year.</p>
+              </div>
+
+            </div>
+
+          </div>
         </div>
 
       </div >

03eaace5419882440c1937a27b638aeb8c18438f
BodartA <a.bodart23@gmail.com>
2024-12-07 12:43:27 +0100

    Minor changes on main page

--- src/App.tsx
+++ src/App.tsx
@@ -7,7 +7,7 @@ function App() {
   return (
     <div className='min-h-screen'>
       <Navbar></Navbar>
-      <div className=''>
+      <div className=' pt-12 sm:pt-16'>
         <p className=''><Link to={"/day01"}>Day 01</Link ></p>
       </div>
     </div>
